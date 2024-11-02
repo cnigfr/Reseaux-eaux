d180fc6a0431e0cc73291239cdc4647ecdb65d06
apasquier <alain.pasquier@eauduponant.fr>
2024-11-02 09:03:02 +0100

    chgt nom schema incendie

--- Standard StaR-Eau/base postgis/08-creation gestion pei.sql
+++ Standard StaR-Eau/base postgis/08-creation gestion pei.sql
@@ -2,7 +2,7 @@
  * 08-creation gestion pei.sql
  *
  * // Created: 2024/07/01 05:48:52
- * // Last modified: 2024/09/25 00:01:31
+ * // Last modified: 2024/11/02 08:15:34
  *
  * ETALABV2 - Alain pour CNIG-2024
  *
@@ -26,21 +26,21 @@
  */
 
 
---création de la gestion des PEI suivant modele AFIGEO
+--création de la gestion des PEI suivant modele afigeo-SDIS
 --suivant https://www.afigeo.asso.fr/modele-minimal-pour-la-donnee-points-d-eau-incendie-22-mai-2018/
 
-CREATE SCHEMA IF NOT EXISTS afigeo_incendie;
-COMMENT ON SCHEMA afigeo_incendie IS 'défense extérieure contre l''incendie';
+CREATE SCHEMA IF NOT EXISTS defense_incendie;
+COMMENT ON SCHEMA defense_incendie IS 'défense extérieure contre l''incendie (DECI) suivant modèle SDIS AFIGEO';
 
 --liste de valeurs incendie
 --type
-CREATE TABLE afigeo_incendie.pei_type (
+CREATE TABLE defense_incendie.pei_type (
   code varchar NOT NULL,
   valeur text NOT NULL,
   description text NULL,
   CONSTRAINT pei_type_pk PRIMARY KEY (code)
 );
-INSERT INTO afigeo_incendie.pei_type (code,valeur,description) VALUES
+INSERT INTO defense_incendie.pei_type (code,valeur,description) VALUES
    ('PI','poteau incendie','poteau d''incendie'),
    ('BI','bouche incendie','Prise d’eau sous pression'),
    ('PA','point aspiration','Point d’aspiration aménagé (point de puisage…)'),
@@ -48,38 +48,38 @@ INSERT INTO afigeo_incendie.pei_type (code,valeur,description) VALUES
    ('NR','non renseigné','non renseigné ou inconnu');
 
 --statut
-CREATE TABLE afigeo_incendie.pei_statut (
+CREATE TABLE defense_incendie.pei_statut (
   code varchar NOT NULL,
   valeur text NOT NULL,
   description text NULL,
   CONSTRAINT pei_statut_pk PRIMARY KEY (code)
 );
-INSERT INTO afigeo_incendie.pei_statut (code,valeur,description) VALUES
+INSERT INTO defense_incendie.pei_statut (code,valeur,description) VALUES
   ('public','public','un P.E.I. public est à la charge du service public de la D.E.C.I. '),
   ('privé','privé','un P.E.I. privé est à la charge de son propriétaire.'),
   ('NR','non renseigné','non renseigné ou inconnu');
 
 ---diametre
-CREATE TABLE afigeo_incendie.pei_diam (
+CREATE TABLE defense_incendie.pei_diam (
   code int2 NOT NULL,
   valeur text NOT NULL,
   description text NULL,
   CONSTRAINT pei_diam_pk PRIMARY KEY (code)
 );
-INSERT INTO afigeo_incendie.pei_diam (code,valeur,description) VALUES
+INSERT INTO defense_incendie.pei_diam (code,valeur,description) VALUES
    ('80','80mm','1 prise de 65'),
    ('100','100mm','2 prises de diamètre 65, 1 prise de diamètre 100'),
    ('150','150mm','2 prises de diamètre 100'),
    ('999','inconnu','non renseigné ou inconnu');
 
 ---source
-CREATE TABLE afigeo_incendie.pei_source (
+CREATE TABLE defense_incendie.pei_source (
   code varchar NOT NULL,
   valeur text NOT NULL,
   description text NULL,
   CONSTRAINT pei_source_pk PRIMARY KEY (code)
 );
-INSERT INTO afigeo_incendie.pei_source (code,valeur,description) VALUES
+INSERT INTO defense_incendie.pei_source (code,valeur,description) VALUES
   ('citerne','citerne','citerne'),
   ('plan_eau','plan d''eau','plan d''eau'),
   ('piscine','piscine','piscine'),
@@ -89,13 +89,13 @@ INSERT INTO afigeo_incendie.pei_source (code,valeur,description) VALUES
 
 --precision
 ---diametre
-CREATE TABLE afigeo_incendie.pei_precision (
+CREATE TABLE defense_incendie.pei_precision (
   code varchar NOT NULL,
   valeur text NOT NULL,
   description text NULL,
   CONSTRAINT pei_precision_pk PRIMARY KEY (code)
 );
-INSERT INTO afigeo_incendie.pei_precision (code,valeur,description) VALUES
+INSERT INTO defense_incendie.pei_precision (code,valeur,description) VALUES
    ('01','01','0 à 1 m'),
    ('05','05','de 1 à 5 m'),
    ('10','10','de 5 à 10 m'),
@@ -103,7 +103,7 @@ INSERT INTO afigeo_incendie.pei_precision (code,valeur,description) VALUES
    ('','inconnu','inconnu');
 
 --table
-CREATE TABLE afigeo_incendie.pei (
+CREATE TABLE defense_incendie.pei (
   insee text NULL, -- Numéro INSEE de la commune
   id_sdis text NOT NULL, -- Identifiant interne du PEI pour le SDIS
   id_gestion text NULL, -- Identifiant interne du PEI pour le gestionnaire
@@ -135,47 +135,47 @@ CREATE TABLE afigeo_incendie.pei (
   geom public.geometry(point, 2154) NULL,
   CONSTRAINT pei_pk PRIMARY KEY (id_sdis)
 );
-CREATE INDEX sidx_pei_geom ON afigeo_incendie.pei USING gist (geom);
+CREATE INDEX sidx_pei_geom ON defense_incendie.pei USING gist (geom);
 ---commentaires
 
-COMMENT ON TABLE afigeo_incendie.pei IS 'points d''eau incendie suivant modele afigeo';
+COMMENT ON TABLE defense_incendie.pei IS 'points d''eau incendie suivant modele defense';
 -- Column comments
-COMMENT ON COLUMN afigeo_incendie.pei.insee IS 'Numéro INSEE de la commune';
-COMMENT ON COLUMN afigeo_incendie.pei.id_sdis IS 'Identifiant interne du PEI pour le SDIS';
-COMMENT ON COLUMN afigeo_incendie.pei.id_gestion IS 'Identifiant interne du PEI pour le gestionnaire';
-COMMENT ON COLUMN afigeo_incendie.pei.nom_gest IS 'Nom du gestionnaire responsable de distribution';
-COMMENT ON COLUMN afigeo_incendie.pei.ref_terr IS 'Numéro ou référence du point d’eau visible sur le terrain';
-COMMENT ON COLUMN afigeo_incendie.pei.type_pei IS 'Type de point d’eau incendie.
+COMMENT ON COLUMN defense_incendie.pei.insee IS 'Numéro INSEE de la commune';
+COMMENT ON COLUMN defense_incendie.pei.id_sdis IS 'Identifiant interne du PEI pour le SDIS';
+COMMENT ON COLUMN defense_incendie.pei.id_gestion IS 'Identifiant interne du PEI pour le gestionnaire';
+COMMENT ON COLUMN defense_incendie.pei.nom_gest IS 'Nom du gestionnaire responsable de distribution';
+COMMENT ON COLUMN defense_incendie.pei.ref_terr IS 'Numéro ou référence du point d’eau visible sur le terrain';
+COMMENT ON COLUMN defense_incendie.pei.type_pei IS 'Type de point d’eau incendie.
 Valeurs possibles : PI, BI, PA, CI';
-COMMENT ON COLUMN afigeo_incendie.pei.type_rd IS 'Précision sur le type de point d’eau incendie défini dans le règlement départemental DECI';
-COMMENT ON COLUMN afigeo_incendie.pei.diam_pei IS 'Diamètre intérieur du poteau ou de la bouche
+COMMENT ON COLUMN defense_incendie.pei.type_rd IS 'Précision sur le type de point d’eau incendie défini dans le règlement départemental DECI';
+COMMENT ON COLUMN defense_incendie.pei.diam_pei IS 'Diamètre intérieur du poteau ou de la bouche
 Valeurs possibles : 80, 100, 150';
-COMMENT ON COLUMN afigeo_incendie.pei.diam_cana IS 'Diamètre de la canalisation exprimé en mm pour les PI et BI';
-COMMENT ON COLUMN afigeo_incendie.pei.source_pei IS 'Source du point d’eau';
-COMMENT ON COLUMN afigeo_incendie.pei.statut IS 'Statut du point d’eau (public, prive)';
-COMMENT ON COLUMN afigeo_incendie.pei.nom_etab IS 'Dans le cas d’un statut privé, nom de l’établissement propriétaire';
-COMMENT ON COLUMN afigeo_incendie.pei.situation IS 'Adresse ou informations permettant de faciliter la localisation du point d’eau sur le terrain.';
-COMMENT ON COLUMN afigeo_incendie.pei.press_dyn IS 'Pression dynamique en bars au débit nominal';
-COMMENT ON COLUMN afigeo_incendie.pei.press_stat IS 'Pression statique en bars';
-COMMENT ON COLUMN afigeo_incendie.pei.debit IS 'Valeur de débit mesuré exprimé en m3/h sous une pression de 1 bar';
-COMMENT ON COLUMN afigeo_incendie.pei.volume IS 'Capacité volumique utile de la source d’eau en m3';
-COMMENT ON COLUMN afigeo_incendie.pei.disponible IS '0 ou 1. Valide à la date de dernière mise à disposition des données';
-COMMENT ON COLUMN afigeo_incendie.pei.date_mes IS 'Date de Date de mise en service du PEI';
-COMMENT ON COLUMN afigeo_incendie.pei.date_maj IS 'Date de dernière mise à jour de la donnée a';
-COMMENT ON COLUMN afigeo_incendie.pei.date_ct IS 'Date du dernier contrôle technique';
-COMMENT ON COLUMN afigeo_incendie.pei.date_ro IS 'Date de la dernière reconnaissance opérationnelle';
-COMMENT ON COLUMN afigeo_incendie.pei.prec IS 'Classes de précision';
-COMMENT ON COLUMN afigeo_incendie.pei.date_dispo IS 'Date de dernier changement d’état de disponibilité';
-COMMENT ON COLUMN afigeo_incendie.pei.x_l93 IS 'x en lambert 93 (précision de 2 décimales)';
-COMMENT ON COLUMN afigeo_incendie.pei.y_l93 IS 'y en lambert 93 (précision de 2 décimales)';
-COMMENT ON COLUMN afigeo_incendie.pei.lon IS 'longitude en degrés décimaux en WGS 84 (précision de 8 décimales)';
-COMMENT ON COLUMN afigeo_incendie.pei.lat IS 'latitude en degrés décimaux en WGS 84 (précision de 8 décimales)';
+COMMENT ON COLUMN defense_incendie.pei.diam_cana IS 'Diamètre de la canalisation exprimé en mm pour les PI et BI';
+COMMENT ON COLUMN defense_incendie.pei.source_pei IS 'Source du point d’eau';
+COMMENT ON COLUMN defense_incendie.pei.statut IS 'Statut du point d’eau (public, prive)';
+COMMENT ON COLUMN defense_incendie.pei.nom_etab IS 'Dans le cas d’un statut privé, nom de l’établissement propriétaire';
+COMMENT ON COLUMN defense_incendie.pei.situation IS 'Adresse ou informations permettant de faciliter la localisation du point d’eau sur le terrain.';
+COMMENT ON COLUMN defense_incendie.pei.press_dyn IS 'Pression dynamique en bars au débit nominal';
+COMMENT ON COLUMN defense_incendie.pei.press_stat IS 'Pression statique en bars';
+COMMENT ON COLUMN defense_incendie.pei.debit IS 'Valeur de débit mesuré exprimé en m3/h sous une pression de 1 bar';
+COMMENT ON COLUMN defense_incendie.pei.volume IS 'Capacité volumique utile de la source d’eau en m3';
+COMMENT ON COLUMN defense_incendie.pei.disponible IS '0 ou 1. Valide à la date de dernière mise à disposition des données';
+COMMENT ON COLUMN defense_incendie.pei.date_mes IS 'Date de Date de mise en service du PEI';
+COMMENT ON COLUMN defense_incendie.pei.date_maj IS 'Date de dernière mise à jour de la donnée a';
+COMMENT ON COLUMN defense_incendie.pei.date_ct IS 'Date du dernier contrôle technique';
+COMMENT ON COLUMN defense_incendie.pei.date_ro IS 'Date de la dernière reconnaissance opérationnelle';
+COMMENT ON COLUMN defense_incendie.pei.prec IS 'Classes de précision';
+COMMENT ON COLUMN defense_incendie.pei.date_dispo IS 'Date de dernier changement d’état de disponibilité';
+COMMENT ON COLUMN defense_incendie.pei.x_l93 IS 'x en lambert 93 (précision de 2 décimales)';
+COMMENT ON COLUMN defense_incendie.pei.y_l93 IS 'y en lambert 93 (précision de 2 décimales)';
+COMMENT ON COLUMN defense_incendie.pei.lon IS 'longitude en degrés décimaux en WGS 84 (précision de 8 décimales)';
+COMMENT ON COLUMN defense_incendie.pei.lat IS 'latitude en degrés décimaux en WGS 84 (précision de 8 décimales)';
 
 --ajout contraintes - optionnelle, à lancer après peuplement et vérification
 /*
-ALTER TABLE afigeo_incendie.pei ADD CONSTRAINT diam_fk FOREIGN KEY (diam_pei) REFERENCES afigeo_incendie.pei_diam(code) ON UPDATE CASCADE;
-ALTER TABLE afigeo_incendie.pei ADD CONSTRAINT precision_fk FOREIGN KEY (prec) REFERENCES afigeo_incendie.pei_precision(code) ON UPDATE CASCADE;
-ALTER TABLE afigeo_incendie.pei ADD CONSTRAINT source_fk FOREIGN KEY (source_pei) REFERENCES afigeo_incendie.pei_source(code) ON UPDATE CASCADE;
-ALTER TABLE afigeo_incendie.pei ADD CONSTRAINT statut_fk FOREIGN KEY (statut) REFERENCES afigeo_incendie.pei_statut(code) ON UPDATE CASCADE;
-ALTER TABLE afigeo_incendie.pei ADD CONSTRAINT type_fk FOREIGN KEY (type_pei) REFERENCES afigeo_incendie.pei_type(code) ON UPDATE CASCADE;
+ALTER TABLE defense_incendie.pei ADD CONSTRAINT diam_fk FOREIGN KEY (diam_pei) REFERENCES defense_incendie.pei_diam(code) ON UPDATE CASCADE;
+ALTER TABLE defense_incendie.pei ADD CONSTRAINT precision_fk FOREIGN KEY (prec) REFERENCES defense_incendie.pei_precision(code) ON UPDATE CASCADE;
+ALTER TABLE defense_incendie.pei ADD CONSTRAINT source_fk FOREIGN KEY (source_pei) REFERENCES defense_incendie.pei_source(code) ON UPDATE CASCADE;
+ALTER TABLE defense_incendie.pei ADD CONSTRAINT statut_fk FOREIGN KEY (statut) REFERENCES defense_incendie.pei_statut(code) ON UPDATE CASCADE;
+ALTER TABLE defense_incendie.pei ADD CONSTRAINT type_fk FOREIGN KEY (type_pei) REFERENCES defense_incendie.pei_type(code) ON UPDATE CASCADE;
 */
